f9beb7e587574f5821c905cf740f42ded45fbf71
Nishant <ndjangra1027@gmail.com>
2023-04-23 04:37:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+const port = 3000
+
+
+
+// Set public folder as static folder for static files
+app.use(express.static(__dirname + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
